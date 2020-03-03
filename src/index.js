3b3c6e0f945c12f2ec265be5aa6d609ca4e61737
keksik77 <lukashevich7tim@gmail.com>
2020-03-04 01:07:55 +0300

    comleted task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let i = 0;
+    while (i < bracketsConfig.length) 
+    {
+      let joinBr = bracketsConfig[i].join('');
+      if (str.includes(joinBr)) 
+      {
+          str = str.replace(joinBr,'');
+          i= 0;
+      } else {
+          i++;
+      }
+    }
+    if(str.length){
+      return false;
+    }else{
+      return true;
+    }  
 }
